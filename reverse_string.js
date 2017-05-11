834c9838fcf62f306ef24687ea4bfea63ca4a7c8
ludralph <raphaelumoh@gmail.com>
2017-05-11 12:13:50 +0100

    completed reverse word function

--- reverse_string.js
+++ reverse_string.js
@@ -2,5 +2,23 @@
 var exports = module.exports = {};
 exports.reverseString = (word) =>
 {
-  
+     // declare reverse word variable
+     let reverseWord = "";
+     // check if word is empty string
+     if (word === ""){
+       return null;
+     }
+     //loop through word from the last character to the first
+     // and append to reverseWord
+     for (let count = word.length-1; count >=0; count--){
+       reverseWord = reverseWord + word[count];
+     }
+     // check if word is equal reverse word
+     if (word === reverseWord){
+       return true;
+     }
+     else{
+       return reverseWord;
+     }
+
 }
